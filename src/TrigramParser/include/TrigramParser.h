cf0d1ace70f71d8e28131893f096c0d5b98a2240
Jeremy Geras <jeremy.geras@alianza.com>
2024-02-05 07:02:28 -0800

    document public interface of TrigramParser

--- src/TrigramParser/include/TrigramParser.h
+++ src/TrigramParser/include/TrigramParser.h
@@ -10,7 +10,18 @@ class TrigramParser
 public:
     TrigramParser() {}
     virtual ~TrigramParser() {}
+    /**
+     * @brief Get the consecutive matching trigrams from two input strings.
+     * Input strings are validated - must be < 100 characters in length
+     * and contain only a-z or A-Z or the space character.
+     * 
+     * @param firstString Valid string.
+     * @param secondString Valid string.
+     * @param trigramPtrs Output param containing the resulting trigrams; should be empty on input.
+     * @return int Returns 0 if input parameters were valid, -1 otherwise.
+     */
     int getTrigrams(const std::string& firstString, const std::string& secondString, std::vector<const char*>& trigramPtrs);
+    
     static bool isInputValid(const std::string& inString);
 };
 }
